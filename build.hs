834af4f46309d7242fa699e46f9f16661393e762
Joel E Carlson <joel.elmer.carlson@gmail.com>
2018-03-28 12:32:19 -0700

    + 1522265540

--- build.hs
+++ build.hs
@@ -1,15 +1,14 @@
 #!/usr/bin/env stack
--- stack script --system-ghc --resolver lts-9.0 --package "process"
-{-# LANGUAGE OverloadedStrings #-}
+-- stack script --system-ghc --resolver lts-9.18 --package "process"
+module Main where
 
-import System.Environment
-import System.Exit
-import System.IO
-import System.Process 
+  import System.Environment
+  import System.Exit
+  import System.IO
+  import System.Process 
 
-main :: IO ()
-main = do
-  run <- callCommand "stack --system-ghc build"
-  putStrLn $ "build: " ++ show run
-  run <- callCommand "stack --system-ghc exec bifrost json/monkey.json"
-  putStrLn $ "run file: " ++ show run
+  main :: IO ()
+  main = do
+    run <- callCommand "stack build"
+    run <- callCommand "stack exec bifrost json/monkey.json"
+    putStrLn $ show run
